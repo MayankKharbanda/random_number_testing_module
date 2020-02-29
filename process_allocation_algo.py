973a00bb26e33b46b85815fbda12e348fdff9a16
MayankKharbanda <kharbanda.mayank@outlook.com>
2020-02-29 21:41:51 +0530

    dieharder alphabit rabbit integrated, documented

--- process_allocation_algo.py
+++ process_allocation_algo.py
@@ -1,45 +1,77 @@
+import config
 
-SNO = 0
-SUITE = 1
-NAME = 2
-ID = 3
-TIME = 4
-SIZE = 5
-
+    
+'''
+This function allocates nearly equally time consuming tests to each of the 
+cores, in the motivation to minimize the wait time of random number generator
+between the running of tests.
+'''
 
 def process_alloc(cores, Tests):
     
-    #checking for running time
+    
+    '''
+    input: number of cores on which tests are to be run,
+            the list of tests to be run.
+    output: A list containing 'n' lists, for 'n' cores, 
+            of tests for running on each core, with core 1
+            containing the fastest tests, and nth core 
+            contains the slowest, generally.
+    NOTE: If running time for even a single test is not
+            available, function returns -1.
+    '''
+    
+    
+    
+    
+    #check if run time exists in the Tests list, else return -1.
     for test in range(len(Tests)):
-        if(Tests[test][TIME]=='None'):
+        if(Tests[test][config.TIME]=='None'):
             return -1
                 
 
+    
+    
     process_list = []       #final list of tests on each processes
 
+
+    
+    #Copy test list so that original list doesn't get affected
     tests_copy = Tests.copy()
 
 
+
+    #create process list for each core.
     for _ in range(cores):
         process_list.append([])
 
 
+    
     #sorting list wrt time to execute
-    tests_copy.sort(key = lambda x:int(x[TIME]))
+    tests_copy.sort(key = lambda x:int(x[config.TIME]))
 
 
+    
     total_time = 0     #summation of wait time of all the processors
 
 
+    
+    
+    #Total time required to execute x-n tests, where x is total number 
+    #of tests, and n is the number of cores in the list.
     if(len(tests_copy)>cores):
-        total_time = sum(int(test[TIME]) for test in tests_copy[:-cores])
+        total_time = sum(int(test[config.TIME]) for test in tests_copy[:-cores])
 
 
-    #wait time for each processor/ time taken to start last test on the process
+    
+    
+    #wait time for each processor/time taken to start last test on the process
     approx_time_each_process = total_time/cores
 
 
-    #adding last test to each process
+    
+    
+    #adding slowest tests to each core list
     for i in range(cores):
         if(len(tests_copy)>0):
             process_list[-(i+1)].insert(0, tests_copy[-1])
@@ -47,14 +79,18 @@ def process_alloc(cores, Tests):
 
 
 
-    #adding tests to all the processes except the first one
+
+
+
+    #adding tests to all the core tests lists except the first one
+    #in greedy algorithm.
     for i in range(cores-1):
         if(len(tests_copy)>0):
-            temp_sum = int(tests_copy[-1][TIME])
+            temp_sum = int(tests_copy[-1][config.TIME])
             process_list[-(i+1)].insert(0, tests_copy[-1])
             del tests_copy[-1]
             if(len(tests_copy)>0):
-                temp_sum = temp_sum+int(tests_copy[0][TIME])
+                temp_sum = temp_sum+int(tests_copy[0][config.TIME])
                 j = 0
                 while(temp_sum < approx_time_each_process):
                     process_list[-(i+1)].insert(j, tests_copy[0])
@@ -62,14 +98,16 @@ def process_alloc(cores, Tests):
                     j = j+1
                     if(len(tests_copy) == 0):
                         break
-                    temp_sum = temp_sum+int(tests_copy[0][TIME])
+                    temp_sum = temp_sum+int(tests_copy[0][config.TIME])
 
 
-    #adding all left over tests to the first processor
+    #adding all left over tests to the first core
     j=0
     while(len(tests_copy)!=0):
         process_list[0].insert(j,tests_copy[0])
         j = j+1
         del tests_copy[0]
+    
+    
     
     return process_list
